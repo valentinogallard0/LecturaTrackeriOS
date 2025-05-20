227a9b2a4a4dcc16f4f185c66d6276040c47960d
valentinogallard0 <al03077250@tecmilenio.mx>
2025-05-20 14:48:40 -0600

    Initial commit

---
+++ LecturaTracker/LecturaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LecturaTrackerApp.swift
+//  LecturaTracker
+//
+//  Created by Valentino De Paola Gallardo on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LecturaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
